7b2de77915ba08435137328cde887abcff096f38
edonssfall <tsarov13leo@gmail.com>
2024-05-19 23:13:25 +0200

    refactor: separate about me in different components

--- src/app/features/portfolio/components/about/about.component.ts
+++ src/app/features/portfolio/components/about/about.component.ts
@@ -2,11 +2,14 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-about',
-  standalone: true,
-  imports: [],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+  educationData = [
+    { name: 'Bachelor\'s Degree', start: '2015', status: 'Complete' },
+    { name: 'Master\'s Degree', start: '2018', status: 'Ongoing' },
+  ];
 
+  displayedColumns: string[] = ['education', 'spacer', 'start', 'status'];
 }
--- src/app/features/portfolio/portfolio.module.ts
+++ src/app/features/portfolio/portfolio.module.ts
@@ -1,9 +1,21 @@
+import {
+  MatCell, MatCellDef,
+  MatColumnDef,
+  MatHeaderCell,
+  MatHeaderCellDef,
+  MatHeaderRow, MatHeaderRowDef,
+  MatRow, MatRowDef,
+  MatTable
+} from "@angular/material/table";
+import { EducationComponent } from './components/about/education/education.component';
 import {NavigationComponent} from './components/navigation/navigation.component';
 import {ProjectsComponent} from "./components/projects/projects.component";
 import {ContactComponent} from "./components/contact/contact.component";
+import {WorkComponent} from './components/about/work/work.component';
 import {PortfolioRoutingModule} from "./portfolio-routing.module";
 import {AboutComponent} from "./components/about/about.component";
 import {HomeComponent} from "./components/home/home.component";
+import {MeComponent} from './components/about/me/me.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatButtonModule} from '@angular/material/button';
@@ -12,16 +24,20 @@ import {ThemeService} from "./services/theme.service";
 import {MatIconModule} from '@angular/material/icon';
 import {MatListModule} from '@angular/material/list';
 import {MatMenuModule} from '@angular/material/menu';
+import {MatRipple} from "@angular/material/core";
 import {RouterModule} from "@angular/router";
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
-import {MatRipple} from "@angular/material/core";
 
 @NgModule({
   declarations: [
     NavigationComponent,
+    EducationComponent,
     PortfolioComponent,
+    AboutComponent,
+    WorkComponent,
     HomeComponent,
+    MeComponent,
   ],
   exports: [
     PortfolioComponent,
@@ -38,8 +54,17 @@ import {MatRipple} from "@angular/material/core";
     CommonModule,
     ProjectsComponent,
     ContactComponent,
-    AboutComponent,
     MatRipple,
+    MatTable,
+    MatHeaderCell,
+    MatCell,
+    MatHeaderRow,
+    MatRow,
+    MatColumnDef,
+    MatHeaderCellDef,
+    MatCellDef,
+    MatHeaderRowDef,
+    MatRowDef,
   ],
   providers: [
     ThemeService,
