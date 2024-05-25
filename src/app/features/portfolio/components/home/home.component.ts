b5aab78f7d6e10496ae2a8f5f17cbd3869c7637b
edonssfall <tsarov13leo@gmail.com>
2024-05-25 07:44:14 +0200

    feat: translate home

--- src/app/features/portfolio/components/home/home.component.ts
+++ src/app/features/portfolio/components/home/home.component.ts
@@ -6,8 +6,8 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  text = 'Web Developer';
-  textArray = ['Web Developer', 'Backend Developer', 'Student', 'Designer'];
+  text = 'web';
+  textArray = ['web', 'backend', 'student', 'designer'];
   currentIndex = 0;
 
   ngOnInit() {
